da03c4b8fb86595acad86c0982f906d0ae8591b8
Marisa Alves <marisa.alves@mail.concordia.ca>
2019-05-01 12:57:29 -0400

    Diabetes dataset reformatting in python

--- my_csv_reader.py
+++ my_csv_reader.py
@@ -2,29 +2,32 @@
 
 import os
 
-file_path = 'housing.data'
-
+file_path = '/Users/marisaalves/Downloads/toys-datasets/diabetes.data'
 if os.path.isfile(file_path):
     print('I have a valid file!!!')
 else:
-    print('Invalid file, I\'ll crash')
+    print('Invalid file, program will crash')
 
-file = open('housing.data')
+file = open('/Users/marisaalves/Downloads/toys-datasets/diabetes.data')
 
 corrected_file = []
+i = 0
 for line in file.readlines():
-    clean_line = line.replace('  ', ' ').replace('  ', ' ').strip()
-    line_values = clean_line.split(' ')
-    corrected_line = []
-    print(line_values)
-    for value in line_values:
-        # for homework:
-        # identify what type of data each value is, and cast it
-        # to the appropriate type, then print the new, properly-typed
-        # list to the screen.
-        # I.e. ['0.04741', '0.00', '11.930', '0', '0.5730', '6.0300', '80.80', '2.5050', '1', '273.0', '21.00', '396.90', '7.88', '11.90']
-        # becomes: [0.04741, 0.0, 11.93, 0, 0.573, 6.03, 80.8, 2.505, 1, 273.0, 21.0, 396.90, 7.88, 11.90]
-    print(corrected_line)
+	clean_line = line.replace("\t",' ').strip()
+	line_values = clean_line.split(' ')
+	corrected_line = []
+	print(line_values)
+	if i > 0:
+		for value in line_values:
+			if (float(value)).is_integer():
+				corrected_line.append(int(value))	 
+			else:
+				corrected_line.append(float(value))
+			print(corrected_line)
+		corrected_file.append(corrected_line)
+	else:
+		corrected_file.append(line_values)
+	i = i+1
 print(corrected_file)
 
 file.close()
