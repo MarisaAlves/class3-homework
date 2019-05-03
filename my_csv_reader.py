f2d02e16d96baa7e65469823460382dd5d6f7963
Marisa Alves <marisa.alves@mail.concordia.ca>
2019-05-03 10:40:54 -0400

    Added a few comments and removed unneccessary print outputs

--- my_csv_reader.py
+++ my_csv_reader.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+#This program will open and reformat the diabetes dataset
 
 import os
 
@@ -16,20 +17,20 @@ for line in file.readlines():
 	clean_line = line.replace("\t",' ').strip()
 	line_values = clean_line.split(' ')
 	corrected_line = []
-	print(line_values)
+#First line of the file are strings, they are the column labels
 	if i > 0:
 		for value in line_values:
 			if (float(value)).is_integer():
 				corrected_line.append(int(value))	 
 			else:
 				corrected_line.append(float(value))
-			print(corrected_line)
 		corrected_file.append(corrected_line)
 	else:
 		corrected_file.append(line_values)
 	i = i+1
 print(corrected_file)
 
+#Reach part of homework, transposing values
 corrected_list = list(map(list,zip(*corrected_file)))
 print(corrected_list)
 
